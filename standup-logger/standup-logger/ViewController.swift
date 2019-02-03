9d3d836f3304fcdbd4f6978bd6fbef7f04760ead
Rob Drimmie <rob_drimmie@manulife.com>
2019-02-03 13:47:59 -0500

    that new project smell

---
+++ standup-logger/standup-logger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  standup-logger
+//
+//  Created by Rob Drimmie on 2019-02-03.
+//  Copyright © 2019 Rob Drimmie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
