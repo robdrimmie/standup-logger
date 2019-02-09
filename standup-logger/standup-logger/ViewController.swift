0d0918e6e9c50f3055df0ba233c4fe85b603a606
Rob Drimmie <rob_drimmie@manulife.com>
2019-02-09 08:52:56 -0500

    Basic screen, CR stubs. Next: copy into formatted text

--- standup-logger/standup-logger/ViewController.swift
+++ standup-logger/standup-logger/ViewController.swift
@@ -9,7 +9,11 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var yesterday: NSTextField!
+    @IBOutlet weak var today: NSTextField!
+    @IBOutlet weak var blockers: NSTextField!
+    @IBOutlet weak var status: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,5 +27,52 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func onStandupDone(_ sender: Any) {
+        let appDelegate = NSApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let standupEntity = NSEntityDescription.entity(forEntityName: "Standup", in: managedContext)
+        
+        let newStandup = NSManagedObject(entity: standupEntity!, insertInto: managedContext)
+        newStandup.setValue(yesterday.stringValue, forKey: "yesterday")
+        newStandup.setValue(today.stringValue, forKey: "today")//
+        newStandup.setValue(blockers.stringValue, forKey: "blockers")
+        newStandup.setValue(Date(), forKey:"created")
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }        
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd mm yyyy"
+        
+        let result = formatter.string(from: newStandup.value(forKey: "created") as! Date)
+        
+        status.stringValue = result
+        
+        foo()
+    }
+    
+    func foo() {
+        let appDelegate = NSApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+//        let standupEntity = NSEntityDescription.entity(forEntityName: "Standup", in: managedContext)
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Standup")
+
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "yesterday") as! String)
+            }
+            
+        } catch {
+            
+            print("Failed")
+        }
+    }
+    
+    
 }
 
